d7747f6922b96b36945393e7b1f7502f53fcf4ad
minuteboss <marviynestanley@gmail.com>
2022-07-09 10:15:16 +0300

    2 commit'

--- skyliqa/settings.py
+++ skyliqa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)@4r*asgss&p7ehcum)1kkc@mg2y8b(3+loiume^*7#9-0eixa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['skyliqa.herokuapp.com','127.0.0.1']
 
